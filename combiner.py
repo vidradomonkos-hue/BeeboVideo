9401c5dae457328eb21f8016643c3a52aff67c14
vidradomonkos-hue <vidradomonkos@gmail.com>
2026-06-23 09:33:02 +0200

    Refactor combiner function for video processing

--- combiner.py
+++ combiner.py
@@ -1,7 +1,7 @@
-import ffmpeg  # Very nice and pretty
-from ffprobe import FFProbe as ffprobe # eew ugle ):<
+import ffmpeg
+from ffprobe import FFProbe as ffprobe
 
-def combiner(videos, output = "video.mp4", SILENCE = "./SILENCE.mp3", print_info = True):
+def combiner(videos, output="video.mp4", SILENCE="./SILENCE.mp3", print_info=True):
     maxRes = (0, 0)
     maxFPS = 0
     filteredVids = []
@@ -13,14 +13,14 @@ def combiner(videos, output = "video.mp4", SILENCE = "./SILENCE.mp3", print_info
             maxFPS = max(vm.framerate, maxFPS)
             filteredVids.append(newVid)
         else:
-            if print_info: print(f"""File {vid} does not contain any video streams, skipping.""")
+            if print_info: print(f"File {vid} does not contain any video streams, skipping.")
     assert len(filteredVids) > 0, "Error: Found no suitable videos."
     
     preparedVids = []
     for filename, vidProps, audProps in filteredVids:
         f = ffmpeg.input(filename).video
-        if (size := (vidProps.width, vidProps.height)) != maxRes:
-            f = f.filter('scale', size = f"{maxRes[0]}x{maxRes[1]}", force_original_aspect_ratio = "decrease")
+        if (int(vidProps.width), int(vidProps.height)) != maxRes:
+            f = f.filter('scale', size=f"{maxRes[0]}x{maxRes[1]}", force_original_aspect_ratio="decrease")
             f = f.filter('pad', maxRes[0], maxRes[1], "(ow-iw)/2", "(oh-ih)/2")
         f = f.filter("setsar", "1")
         preparedVids.append(f)
@@ -29,11 +29,22 @@ def combiner(videos, output = "video.mp4", SILENCE = "./SILENCE.mp3", print_info
         else:
             preparedVids.append(ffmpeg.input(SILENCE).audio)
     
-    final = ffmpeg.concat(*preparedVids, n = len(filteredVids), v = 1, a = 1).output(output).global_args("-y").global_args("-vsync", "2").global_args("-hide_banner").global_args("-loglevel", "error")
+    final = (
+        ffmpeg
+        .concat(*preparedVids, n=len(filteredVids), v=1, a=1)
+        .output(output)
+        .global_args("-y")
+        .global_args("-vsync", "2")
+        .global_args("-hide_banner")
+        .global_args("-loglevel", "error")
+    )
     final.run()
     if print_info: print(f"Finished! Exported video ({maxRes[0]}x{maxRes[1]}p{maxFPS})")
 
 if __name__ == "__main__":
     from os import listdir
     from sys import argv
-    combiner(argv[2:] if len(argv) > 2 else sorted(["videos/"+i for i in listdir("videos")]), argv[1] if len(argv) > 1 else "video.mp4")
+    combiner(
+        argv[2:] if len(argv) > 2 else sorted(["videos/" + i for i in listdir("videos")]),
+        argv[1] if len(argv) > 1 else "video.mp4"
+    )
